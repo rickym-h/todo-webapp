e915e142bde27f0bcc8bfaacb9054d19e0572cf8
rickym-h <richardhull11@gmail.com>
2022-08-05 20:09:08 +0100

    Updates

---
+++ precache-manifest.5f36bc7640778fe93954799ead9205da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd290f768816802c9b7b",
+    "url": "/static/css/main.2dabdef6.chunk.css"
+  },
+  {
+    "revision": "bd290f768816802c9b7b",
+    "url": "/static/js/main.bd290f76.chunk.js"
+  },
+  {
+    "revision": "682b0dce85d9b9a8d8f2",
+    "url": "/static/js/1.682b0dce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66e360adea879dd1f4c439ccd2b4887e",
+    "url": "/index.html"
+  }
+];
